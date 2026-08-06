a57e3be15dd46ac595db51753edc11728498e360
hyena-heckler <shettyharsh248@gmail.com>
2026-08-05 20:43:01 -0400

    Testing SEO link

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,7 @@
 import {store} from "../store.js";
 import {updateUser, fetchCache} from "../services/api.js";
 import {computeStatistics} from "../components/statistics-adder.js";
-import data from "./Data.json";
+//import data from "./Data.json";
 
 export function setupLogin() {
   const loginForm = document.getElementById("panel__form")
@@ -22,15 +22,15 @@ export function setupLogin() {
       }
 
       store.user = username;
-      // let status = await updateUser();
-      // if (status == "failed") {
-      //   store.user = null;
-      //   alert("Invalid Username");
-      //   return
-      // }
+      let status = await updateUser();
+      if (status == "failed") {
+        store.user = null;
+        alert("Invalid Username");
+        return
+      }
 
-      // await fetchCache();
-      store.cache = data;
+      await fetchCache();
+      // store.cache = data;
 
       addDateDropdown();
 
